9bf9d3aa85c2bf718b17e1b91c8910b879b69c07
bgossele <brecht.gossele@gmail.com>
2014-12-12 09:50:55 +0100

    header files

---
+++ button.h
@@ -0,0 +1,9 @@
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+
+#include "component_pub.h"
+DECLARE_LOOCI_COMPONENT(button);
+
+#endif /* TESTCOMPONENT_H_ */
---
+++ potentio.h
@@ -0,0 +1,8 @@
+#ifndef POTENTIO_H
+#define POTENTIO_H
+
+
+#include "component_pub.h"
+DECLARE_LOOCI_COMPONENT(potentio);
+
+#endif /* TESTCOMPONENT_H_ */
